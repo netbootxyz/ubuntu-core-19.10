d31cd7bcf9236888ff136391d917ab99cc649f3c
thelamer <ryankuba@gmail.com>
2019-12-18 17:00:44 -0800

    updating 19.10 default kernel to use liveCD and modlayer logic

---
+++ settings.sh
@@ -0,0 +1,8 @@
+URL="http://releases.ubuntu.com/eoan/ubuntu-19.10-desktop-amd64.iso.torrent"
+TYPE=torrent
+CONTENTS="\
+casper/initrd|initrd
+casper/vmlinuz|vmlinuz"
+EXTRACT_INITRD="true"
+INITRD_NAME="initrd"
+INITRD_TYPE="xz"
